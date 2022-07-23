20b1f784c295b86db3990dcb9bcbbbe2a3d56c0b
analogdev.eth <dejijolaoluwa@gmail.com>
2022-07-23 05:11:24 +0100

    added script to deploy and verify contract

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,10 +1,19 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "dotenv/config";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
   networks: {
-    hardhat: {}
+    hardhat: {},
+    mumbai: {
+      url: process.env.MUMBAI_URL ?? "",
+      chainId: 80001,
+      accounts: [`0x${process.env.PRIVATE_KEY}`]
+    }
+  },
+  etherscan: {
+    apiKey: process.env.MUMBAI_POLYGONSCAN_KEY ?? ""
   }
 };
 
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,18 +1,32 @@
-import { ethers } from "hardhat";
+import { ContractFactory, Contract } from "ethers";
+import hre, { ethers } from "hardhat";
+
+async function delay(time: number) {
+  return new Promise((resolve) => setTimeout(resolve, time * 1000));
+}
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  const metadataURI = "ipfs://QmYc15Cg9mv5UMnmC31oR4usXJs35n7Bn3DDWtfy9fyYV3/";
+  const LW3PunksCF: ContractFactory = await ethers.getContractFactory(
+    "LW3Punks"
+  );
+  const LW3PunksContract: Contract = await LW3PunksCF.deploy(metadataURI);
+  await LW3PunksContract.deployed();
 
-  const lockedAmount = ethers.utils.parseEther("1");
+  console.log(`LW3Punks contract deployed at ${LW3PunksContract.address}`);
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  // verify contract on mumbai polygonscan
+  const delayTime = 60;
+  console.log(
+    `Waiting ${delayTime} seconds for Mumbai Polygonscan to index LW3Punks contract`
+  );
+  await delay(delayTime);
 
-  await lock.deployed();
-
-  console.log("Lock with 1 ETH deployed to:", lock.address);
+  await hre.run("verify:verify", {
+    address: LW3PunksContract.address,
+    contract: "contracts/LW3Punks.sol:LW3Punks",
+    constructorArguments: [metadataURI],
+  });
 }
 
 // We recommend this pattern to be able to use async/await everywhere
